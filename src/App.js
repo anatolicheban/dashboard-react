d352317f32b6d3671309860d47079185f40a9b45
anatolicheban <107035461+anatolicheban@users.noreply.github.com>
2022-09-03 15:38:00 +0300

    all 3 pages are done

--- src/App.js
+++ src/App.js
@@ -54,8 +54,9 @@ const App = () => {
             </div>
           )}
           <div
-            className={`dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu ? "md:ml-72" : "flex-2"
-              }`}
+            className={`dark:bg-main-bg bg-main-bg min-h-screen w-full ${
+              activeMenu ? "md:ml-72" : "flex-2"
+            }`}
           >
             <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
               <Navbar />
